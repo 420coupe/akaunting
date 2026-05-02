e97c284a54c70e62655195e10353dea4140e01e3
420Coupe <420coupe@protonmail.com>
2026-05-02 14:31:40 -0400

    fix(password): initialize password input with provided value for better user experience

--- resources/views/components/form/input/password.blade.php
+++ resources/views/components/form/input/password.blade.php
@@ -1,5 +1,5 @@
-<div x-data="{ showPassword: false, password: '' }" x-cloak>
-    <input 
+<div x-data="{ showPassword: false, password: '{{ addslashes($value ?? '') }}' }" x-cloak>
+    <input
         type="password"
         x-bind:class="password ? 'ltr:pr-9 rtl:pl-9' : 'ltr:pr-0 rtl:pl-0'"
         x-bind:type="showPassword ? 'text' : 'password'"
@@ -23,12 +23,12 @@
         {{ $attributes->except(['placeholder', 'disabled', 'required', 'readonly', 'v-error', 'v-error-message']) }}
     />
 
-    <button 
-        type="button" 
+    <button
+        type="button"
         class="absolute inset-y-0 top-7 ltr:right-0 rtl:left-0 flex items-center ltr:pr-3 rtl:pl-3 text-gray-400"
         x-on:click="showPassword = !showPassword" x-show="password"
     >
         <span x-show="!showPassword" class="material-icons-outlined text-gray-500 text-lg">visibility_off</span>
-        <span x-show="showPassword" class="material-icons-outlined text-gray-500 text-lg">visibility</span>       
+        <span x-show="showPassword" class="material-icons-outlined text-gray-500 text-lg">visibility</span>
     </button>
 </div>
